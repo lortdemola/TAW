1737735265707cc0b1f443da4df891b5096128d5
lortdemola <97162568+lortdemola@users.noreply.github.com>
2023-06-15 17:54:24 +0200

    login fix

--- E_hop/E_hop/src/app/components/signup/signup.component.ts
+++ E_hop/E_hop/src/app/components/signup/signup.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {AuthService} from '../../services/auth.service';
 import {Router} from "@angular/router";
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'signup',
@@ -12,19 +13,33 @@ export class SignupComponent implements OnInit {
   public credentials = {
     name: '',
     email: '',
-    password: '',
-  };
+    password: '', passwordRE: ''
 
-  constructor(private authService: AuthService, public router: Router) {
+  };
+  isSubmitted = false;
+
+  constructor(private authService: AuthService, public router: Router,private formBuilder: FormBuilder) {
   }
 
   ngOnInit() {
+
   }
 
   create() {
+    this.isSubmitted = true;
+
+    if (!this.credentials.email || !this.credentials.name || !this.credentials.password || this.credentials.password !=this.credentials.passwordRE ) {
+
+      return;
+    }
     this.authService.createOrUpdate(this.credentials).subscribe((result) => {
       return result;
     });
     this.router.navigate(['/']);
+  }
+
+  passwordPatternValid() {
+    const pattern = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{5,15}$/;
+    return pattern.test(this.credentials.password) && this.credentials.password?.length >= 5 && this.credentials.password?.length <= 15;
   }
 }
